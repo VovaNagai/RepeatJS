b780f70162cab7013e724d12e2269889bb9ff7a8
Vova Nagai <ghostfalcon.kr@gmail.com>
2018-09-19 17:30:07 +0300

    v1
    add lesson1 and yet hw1

---
+++ lesson1/js/user.js
@@ -0,0 +1,28 @@
+// $(document).ready(function() {
+//     // var name = "Vova";
+//     // const age = 20;
+//     // const pay = 3000;
+//     // // console.log(name + " " + age + " years " + pay +"$" + " " + true)
+//     // console.log("name: " + name);
+//     // console.log("age: " + age);
+//     // console.log("pay: " + pay + "$");
+//     // console.log("info: " + true);
+//
+//
+// });
+
+// var user = {
+//     name: "Vova",
+//     status: {
+//        txt: "easy code student"
+//     }
+// };
+// console.log(user.name + " " + user.status.txt);
+
+//масивы
+
+// var user = {
+//     name: ['Vova', 'Paha']
+// };
+// console.log(user.name[1] + " " + user.name[0]);
+
