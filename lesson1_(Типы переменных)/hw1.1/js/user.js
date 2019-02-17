07544e8489b83f0dafc7d25cb4bb4a54ae013d7a
Vova Nagai <ghostfalcon.kr@gmail.com>
2019-02-17 15:05:29 +0200

    add task

--- lesson1_(Типы переменных)/hw1.1/js/user.js
+++ lesson1_(Типы переменных)/hw1.1/js/user.js
@@ -1,14 +1,14 @@
 // +1) Создать HTML страницу и подключить к ней файл скрипта
-
+//
 // +2) В файле скрипта создать 2 переменные, которые будут получать данные от пользователя:
 //     ·        Первая будет спрашивать "Ваш бюджет?"
 // ·        Вторая "Название вашего магазина?"
-//     var ourBudget = prompt('Ваш бюджет?', '');
-//     console.log('Ваш бюджет: ' + ourBudget);
-//
-//     var nameShop = prompt('Название вашего магазина?', '');
-//     console.log('Название вашего магазина: ' + nameShop);
+var money = prompt('Ваш бюджет?', '');
+// console.log('Ваш бюджет: ' + ourBudget);
 
+var name = prompt('Название вашего магазина?', '');
+// console.log('Название вашего магазина: ' + nameShop);
+let time = 19;
 
 // +3) Создать объект mainList, который будет содержать такие данные:
 //     ·        Бюджет
@@ -17,29 +17,41 @@
 // ·        Объект с сотрудниками employers
 // ·        Свойство open
 
-// var mainList = {
-//   budget: 4000,
-//   nameShop: 'ShopEveryDay',
-//   shopGoods: ['banana', 'onion', 'cucumber', 'apple'],
-//   employers: {
-//       Vova: 21,
-//       Roma: 20,
-//       Victor: 27
-//   },
-//   open: true
-// };
+let mainList = {
+    budget: money,
+    nameShop: name,
+    shopGoods: [],
+    employers: {},
+    open: true
+};
+
 // +4) Задать пользователю 3 раза вопрос "Какой тип товаров будем продавать?" и записать ответы в массив shopGoods
-// var questionAboutSeld1 = prompt('Какой тип товаров будем продавать?', '');
-// var questionAboutSeld2 = prompt('Какой тип товаров будем продавать?', '');
-// var questionAboutSeld3 = prompt('Какой тип товаров будем продавать?', '');
-//
-// let arrSeld = [questionAboutSeld1, questionAboutSeld2, questionAboutSeld3];
-// console.log('Ваши типы товаров для продаж: ' + arrSeld);
+for(let i = 0; i < 5; i++) {
+    let a = prompt('Какой тип товаров будем продавать?');
+
+    if((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50) {
+        console.log('Good!');
+        mainList.shopGoods[i] = a;
+    } else {
+        console.log('Error')
+    }
+}
+
+if(time < 0) {
+    console.log('Такого не может быть!')
+} else if(time > 8 && time < 20) {
+    console.log('Время работать')
+} else if(time < 24) {
+    console.log('Уже поздно!')
+} else {
+    console.log('В сутках только 24 часа');
+}
 
 // +5) Вывести на экран пользователя бюджет на 1 день (брать месяц за 30 дней)
-// var myBudget = prompt('Введите сумму вашего бюджета на месяц:', '');
-// let mountsDay = 30;
-//
-// alert('Ваш бюджет на месяц: ' + Math.round(myBudget / mountsDay) + ' грн.');
+
+let mountsDay = 30;
+
+alert('Ваш бюджет на месяц: ' + Math.round(mainList.budget / mountsDay) + ' грн.');
 // +6) Проверить на наличие ошибок
 // +7) Создать свой репозиторий на GitHub и поместить туда папку с первым заданием
+console.log(mainList);
